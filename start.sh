3272e55213be4d8c6b4c9ba1402c3a15e0777122
M. Clay Johns <gwpc114@gmail.com>
2021-01-25 23:25:49 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,8 @@ set -e
 export AWS_ACCESS_KEY_ID=$KEY
 export AWS_SECRET_ACCESS_KEY=$SECRET
 export AWS_DEFAULT_REGION=$REGION
+export PARAMS=$PARAMS
+export REVERSE=$REVERSE
 
 if [[ "$1" == 'now' ]]; then
     exec /sync.sh
